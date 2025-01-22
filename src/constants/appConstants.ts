aa41e0dbbe20c24e5c527f4ac1ad1aa8e3631b46
tootsydesh <112065547+tootsydesh@users.noreply.github.com>
2025-01-22 17:07:23 +0700

    Update appConstants.ts update user_id

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -38,7 +38,7 @@ export const TEXT_MIME_TYPES = [
 
 export const APP_CONSTANTS = {
   TOKEN_KEY: "token", // 로컬 스토리지에 저장될 JWT 키
-  USER_ID: "myapp_user_id",
+  USER_ID: "user_id",
   CONVERSATION_ID: "conversation_id",
   BASE_URL: "http://127.0.0.1:5000", // Flask 백엔드의 기본 URL
   TOKEN_EXPIRATION: 3600, // 토큰 만료 시간 (초 단위)
